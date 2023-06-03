12ccc5e910930e8b7f754d53bc76474a86cd2054
Blake Stoller <blake.stoller01@gmail.com>
2023-06-02 23:19:33 -0400

    work on service

---
+++ travlr/app_admin/src/app/services/trip-data.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Trip} from '../models/trip';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TripDataService {
+
+  constructor(private http: Http) { }
+
+  private apiBaseUrl = 'http://localhost:3000/api';
+
+  public getTrips() {
+    console.log('Inside TripDataService#getTrips');
+    return this.http.get(`${this.apiBaseUrl}trips`)
+      .toPromise()
+      .then(x => x.json() as Trip[])
+      .catch(this.handleError);
+  }
+
+  private handleError(error:any) {
+    console.error('Something has gone wrong', error);
+    return Promise.reject(error.message || error);
+  }
+}
--- travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
+++ travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,18 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { trips } from '../data/trips';
+import { Component, OnInit } from "@angular/core";
+import { trips } from "../data/trips";
+import { Trip } from "../models/trip";
+import { TripDataService } from "../services/trip-data.service";
 
 @Component({
-  selector: 'app-trip-listing',
-  templateUrl: './trip-listing.component.html',
-  styleUrls: ['./trip-listing.component.css']
+  selector: "app-trip-listing",
+  templateUrl: "./trip-listing.component.html",
+  styleUrls: ["./trip-listing.component.css"],
 })
 export class TripListingComponent implements OnInit {
+  constructor(private tripDataService: TripDataService) {}
 
-  constructor() { }
+  trips: Trip[];
 
-  trips : Array<any> = trips;
+  message: string;
 
-  ngOnInit() {
+  private getTrips() {
+    console.log("Inside TripListingComponent#getTrips");
+    this.message = "Searching for trips";
+    this.tripDataService.getTrips().then((trips) => {
+      this.message = trips.length > 0 ? "" : "No trips found";
+      this.trips = trips;
+    });
   }
 
+  ngOnInit() {
+    this.getTrips();
+  }
 }
